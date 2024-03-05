6b94a6f3b51eac5bbadbab90d7b2b2e24d5e581f
copilot-defender <132240783+copilot-defender@users.noreply.github.com>
2024-03-04 21:40:14 -0800

    adding

---
+++ dependabotNugetTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var obj = new { message = "Hello, World!" };
+        //string json = JsonConvert.SerializeObject(obj);
+        // Console.WriteLine(json);
+    }
+}
